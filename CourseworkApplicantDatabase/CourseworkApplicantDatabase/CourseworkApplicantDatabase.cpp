90897ae3e0d606c5929c47946906e9e638f290bc
lop223 <dolgijmaks3@gmail.com>
2025-08-25 13:49:57 +0300

    create empty console project from VS

---
+++ CourseworkApplicantDatabase/CourseworkApplicantDatabase/CourseworkApplicantDatabase.cpp
@@ -0,0 +1,8 @@
+﻿//! CourseworkApplicantDatabase - The project was created for a coursework on the topic "Enrollment Database Management System". C++
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
